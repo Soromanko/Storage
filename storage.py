7b199736aa815783d8b2be8e4601c99d6377fd8d
Soromanko <samuel.soroman@email.cz>
2025-03-21 20:53:03 +0100

    Initial commit + product search

---
+++ storage.py
@@ -0,0 +1,77 @@
+products = [
+    {
+        "name": "Audi",
+        "price": 50
+    },
+    {
+        "price": 30,
+        "name": "BMW",
+    }
+]
+
+
+def print_products():
+    for product in products:
+        print(f"Název produktu: {product['name']}, cena: {product['price']}$")
+
+
+def add_product():
+    product_name = input("Název produktu:")
+    product_price = input("Cena produktu:")
+    product2 = {
+        'name': product_name,
+        'price': product_price
+    }
+
+    products.append(product2)
+
+
+def find_product():
+    product_find = input("Zadej název produktu: ").lower()
+
+    productFound = [product for product in products if product['name'].lower().startswith(product_find)]
+
+    if productFound:
+        print("Výsledek vyhledávání:")
+        for product in productFound:
+            print(f"Název: {product['name']}, Cena: {product['price']}$")
+    else:
+        print("Produkt nenalezen")
+
+
+
+def menu():
+    print("Vítej ve skladu")
+    print("###############\n")
+    print("1. Výpis položek")
+    print("2. Přidání položky")
+    print("3. Vyhledat produkt(y)\n")
+
+    choice = int(input("Volba: "))
+
+    if choice == 1:
+        print("Položky na skladě jsou:")
+        print_products()
+        print("")
+        menu()
+
+    elif choice == 2:
+        print("Přidání položky:")
+        add_product()
+        print("")
+        menu()
+
+
+    elif choice == 3:
+        print("Vyhledání produktu:")
+        find_product()
+        print("")
+        menu()
+
+
+    else:
+        print("Zadal jsi špatně!\n")
+        menu()
+
+
+menu()
